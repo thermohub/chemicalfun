be1fb8f0c8ebed1b987815eb1cd44ce4eaafd95b
Michael T. Ringel <linktoger@gmx.net>
2025-07-03 10:14:30 +0200

    Updated install-dependencies.bat to enable WASM
    Updated CMakeLists.txt to easily detect the EMSCRIPTEN environment to
    exclude all unnecessary build options.

--- install-dependencies.bat
+++ install-dependencies.bat
@@ -5,6 +5,7 @@
 
 @echo off
 setlocal enableextensions
+setlocal enabledelayedexpansion
 :: Installing dependencies needed to build CemicalFun on WINDOWS
 :: (actually we won't install anything, we will just clone and build files in the directory)
 
@@ -27,23 +28,46 @@ mkdir extern
 
 cd extern
 
-IF EXIST "%~dp0extern\installed\" (
-echo Folder installed already exists
+IF EXIST "%~dp0extern\installedwasm\" (
+echo Folder installedwasm already exists
 ) ELSE (
-echo Creating folder installed
-mkdir installed
+echo Creating folder installedwasm
+mkdir installedwasm
 )
 
-IF NOT EXIST "%~dp0extern\installed\include\nlohmann\json.hpp" (
+IF DEFINED EMSDK (
+:: Set CMake command line args
+REM We don't need to build the *.pyd with emscripten on Windows because we would rather build that with pyodide on linux
+REM echo EMSDK defined
+REM "%EMSDK_PYTHON%" -c "import sysconfig; print(sysconfig.get_path('include'))" >> Output1.txt
+REM for /f "usebackq delims=" %%a in ("%~dp0extern\Output1.txt") do set "PINCDIR=%%a"
+REM DEL Output1.txt
+REM "%EMSDK_PYTHON%" -c "import pathlib; import sys; v = sys.version.split('.'); p = pathlib.Path(r'%EMSDK_PYTHON%'); print(pathlib.Path(p.parent, p.stem + v[0] + v[1] + '.dll'))" >> Output1.txt
+REM for /f "usebackq delims=" %%a in ("%~dp0extern\Output1.txt") do set "PLIBDIR=%%a"
+REM DEL Output1.txt
+REM "%EMSDK_PYTHON%" -c "import pathlib; import sys; p = pathlib.Path(r'%EMSDK_PYTHON%'); print(p.parent)" >> Output1.txt
+REM for /f "usebackq delims=" %%a in ("%~dp0extern\Output1.txt") do set "PROOTDIR=%%a"
+REM DEL Output1.txt
+REM SET "CMAKE_ARGUMENTS=-DCMAKE_TOOLCHAIN_FILE="%EMSDK%/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake" -DCMAKE_CROSSCOMPILING_EMULATOR="%EMSDK_NODE%" -DPython_ROOT_DIR="!PROOTDIR!" -DPython_EXECUTABLE:FILEPATH="%EMSDK_PYTHON%" -DPython_INCLUDE_DIR="!PINCDIR!" -DPython_LIBRARY="!PLIBDIR!" -G Ninja"
+REM Note: For whatever reason, calling emcmake cmake ... messes up the paths, so use the Toolchainfile below
+SET "CMAKE_ARGUMENTS=-DCMAKE_TOOLCHAIN_FILE="%EMSDK%/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake" -DCMAKE_CROSSCOMPILING_EMULATOR="%EMSDK_NODE%" -G Ninja"
+SET "INSTALLPATHROOT=%~dp0extern\installedwasm"
+) ELSE (
+echo EMSDK not defined
+SET "CMAKE_ARGUMENTS= "
+SET "INSTALLPATHROOT=%~dp0extern\installed"
+)
+
+IF NOT EXIST "%INSTALLPATHROOT%\include\nlohmann\json.hpp" (
 :: Getting nlohmann/json library
 echo Downloading json library && ^
 git clone https://github.com/nlohmann/json.git --depth 1 && ^
 cd json && ^
 echo Making JSON library && ^
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DJSON_BuildTests=OFF -DJSON_MultipleHeaders=ON && ^
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DJSON_BuildTests=OFF -DJSON_MultipleHeaders=ON %CMAKE_ARGUMENTS% && ^
 cmake --build build/ && ^
-echo Installing in "%~dp0\extern\installed" && ^
-cmake --install build/ --prefix "%~dp0extern\installed" && ^
+echo Installing in "%INSTALLPATHROOT%" && ^
+cmake --install build/ --prefix "%INSTALLPATHROOT%" && ^
 cd "%~dp0extern" && ^
 rmdir json /s /q
 
@@ -55,15 +79,15 @@ cd "%~dp0extern"
  
 :: Eigen3 math library (added for building and installing xGEMS)
 :: if not installed in /usr/local/include/eigen3)
-IF NOT EXIST "%~dp0extern\installed\include\eigen\" (
+IF NOT EXIST "%INSTALLPATHROOT%\include\eigen3\\" (
 :: Building eigen library
 echo Downloading eigen library
 git clone https://gitlab.com/libeigen/eigen.git -b 3.4.0 --depth 1 && ^
 cd eigen && ^
-cmake -S . -B build && ^
+cmake -S . -B build %CMAKE_ARGUMENTS% && ^
 cmake --build build/ && ^
-echo Installing in "%~dp0extern\installed" && ^
-cmake --install build/ --prefix "%~dp0extern\installed" && ^
+echo Installing in "%INSTALLPATHROOT%" && ^
+cmake --install build/ --prefix "%INSTALLPATHROOT%" && ^
 cd "%~dp0extern" && ^
 rmdir eigen /s /q
 
@@ -75,14 +99,14 @@ cd "%~dp0extern"
 
 :: spdlog 
 :: if no spdlog installed in /usr/local/include/spdlog (copy only headers)
-IF NOT EXIST "%~dp0extern\installed\include\spdlog\" (
+IF NOT EXIST "%INSTALLPATHROOT%\include\spdlog\\" (
 :: Building spdlog library
 echo Downloading eigen library
-git clone https://github.com/gabime/spdlog -b v1.11.0 --depth 1 && ^
+git clone https://github.com/gabime/spdlog --depth 1 && ^
 cd spdlog && ^
 cd include && ^
-IF NOT EXIST "%~dp0extern\installed\include\spdlog\" mkdir "%~dp0extern\installed\include\spdlog" && ^
-xcopy /s/e spdlog "%~dp0extern\installed\include\spdlog" && ^
+IF NOT EXIST "%INSTALLPATHROOT%\include\spdlog\" mkdir "%INSTALLPATHROOT%\include\spdlog" && ^
+xcopy /s/e spdlog "%INSTALLPATHROOT%\include\spdlog" && ^
 cd "%~dp0extern" && ^
 rmdir spdlog /s /q
 
@@ -90,21 +114,25 @@ rmdir spdlog /s /q
 echo spdlog already exists
 )
 
+REM We don't need this for WASM if we don't use the pyodide environemnt, which only works on linux currently
+IF NOT DEFINED EMSDK (
 ::Pybind11
-IF NOT EXIST "%~dp0extern\installed\include\pybind11\" (
+IF NOT EXIST "%INSTALLPATHROOT%\include\pybind11\\" (
 :: Building spdlog library
 echo Downloading pybind11 library
 git clone https://github.com/pybind/pybind11.git --depth 1 && ^
 cd pybind11 && ^
-cmake -S . -B build -DPYBIND11_TEST=OFF && ^
+cmake -S . -B build -DPYBIND11_TEST=OFF %CMAKE_ARGUMENTS% && ^
 cmake --build build/ && ^
-echo Installing in "%~dp0extern\installed" && ^
-cmake --install build/ --prefix "%~dp0extern\installed" && ^
+echo Installing in "%INSTALLPATHROOT%" && ^
+cmake --install build/ --prefix "%INSTALLPATHROOT%" && ^
 cd "%~dp0extern" && ^
 rmdir pybind11 /s /q
 
 ) ELSE (
 echo pybind11 already exists
 )
-
+)
 cd "%~dp0"
+endlocal
+endlocal
